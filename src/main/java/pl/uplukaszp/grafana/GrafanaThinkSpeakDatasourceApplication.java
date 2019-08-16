7c2b7555f70ac076d08200b118cf6bc6cedc93c3
Łukasz <uplukaszp1@gmail.com>
2019-08-16 21:54:48 +0200

    Init Commit

---
+++ src/main/java/pl/uplukaszp/grafana/GrafanaThinkSpeakDatasourceApplication.java
@@ -0,0 +1,13 @@
+package pl.uplukaszp.grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaThinkSpeakDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaThinkSpeakDatasourceApplication.class, args);
+	}
+
+}
